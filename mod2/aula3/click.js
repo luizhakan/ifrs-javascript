5bc2be61bea981ad0d601fe7c9290a6c0a641bae
luiz <uefibiosxx@gmail.com>
2022-03-19 13:48:20 -0300

    Módulo 2

---
+++ mod2/aula3/click.js
@@ -0,0 +1,9 @@
+function umClique() {
+  document.getElementById("imagem").src = "sam.gif";
+  document.getElementById("log").innerHTML += "troca para o sam <br>";
+}
+
+function doisCliques() {
+  document.getElementById("imagem").src = "dean.gif";
+  document.getElementById("log").innerHTML += "troca para o dean <br>";
+}
---
+++ mod2/aula4/input.js
@@ -0,0 +1,27 @@
+function resetei(){
+    document.getElementById('log').innerHTML += 'resetei <br>'
+}
+
+function saiDoCampo(){
+    document.getElementById('log').innerHTML += 'saí do campo <br>'
+}
+
+function mudei(){
+    document.getElementById('log').innerHTML += 'mudei o conteúdo do campo<br>'
+}
+
+function entreiNoCampo(){
+    document.getElementById('log').innerHTML += 'entrei no campo <br>'
+}
+
+function selecionei(){
+    document.getElementById('log').innerHTML += 'selecionei o conteúdo do campo <br>'
+}
+
+function teclaBaixo(){
+    document.getElementById('log').innerHTML += 'apertei a tecla para baixo <br>'
+}
+
+function teclacima(){
+    document.getElementById('log').innerHTML += 'apertei a tecla para cima <br>'
+}
